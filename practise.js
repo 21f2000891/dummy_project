16c114c4b748a2b139775c265f1e97c1503bfa75
21f2000891 <21f2000891@ds.study.iitm.ac.in>
2025-02-18 22:14:45 +0530

    test

--- practise.js
+++ practise.js
@@ -1,4 +1,9 @@
 let s = "hello world";
 // console.log(s);
-let st = `${s} man!`;
-console.log(st);
+
+// let st = `${s} man!`;
+// console.log(st);
+const x = [1,2,3,4];
+for(let j in x){
+    console.log(j);
+}
