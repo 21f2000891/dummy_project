baccbc5af562e1e9ad150fc24ed3cacc3acf348b
21f2000891 <21f2000891@ds.study.iitm.ac.in>
2025-02-20 14:12:02 +0530

    test

--- practise.js
+++ practise.js
@@ -1,9 +1,24 @@
-let s = "hello world";
+// let s = "hello world";
 // console.log(s);
 
 // let st = `${s} man!`;
-// console.log(st);
-const x = [1,2,3,4];
-for(let j in x){
-    console.log(j);
-}
+// // console.log(st);
+// const x = [1,2,3,4];
+// for(let j of x){
+//     console.log(j);
+// }
+// let v = {};
+
+// console.log(v)
+// console.log(v.value)
+// // const x
+// // console.log(x)
+
+// function abc(){
+//     var a = 10;
+//     console.log(a);
+// }
+// // abc();
+
+const c = 10 ;  
+  console.log(c);
